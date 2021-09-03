56c8ed077fdd23f249e636aeaef9cd7bba1f2939
Matthias Stangl <fazmaz@gmx.de>
2021-09-03 11:27:30 +0200

    Spring init

---
+++ backend/src/test/java/de/mcstangl/projectplanner/ProjectPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mcstangl.projectplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

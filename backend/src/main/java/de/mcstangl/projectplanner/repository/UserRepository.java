d365bc395a72851ad1513e7808d54005f42505fe
Matthias Stangl <fazmaz@gmx.de>
2021-09-06 11:14:47 +0200

    Add UserRepository

---
+++ backend/src/main/java/de/mcstangl/projectplanner/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package de.mcstangl.projectplanner.repository;
+
+import de.mcstangl.projectplanner.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}

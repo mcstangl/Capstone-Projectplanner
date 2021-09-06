e77c6d9e4eca3fd0dc92f15e161b2a0eb9cfb7a6
Matthias Stangl <fazmaz@gmx.de>
2021-09-06 09:33:56 +0200

    Add size variables to GlobalStyle

--- frontend/src/components/GlobalStyle.tsx
+++ frontend/src/components/GlobalStyle.tsx
@@ -1,14 +1,22 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+  :root {
+    --size-xs: 4px;
+    --size-s: 8px;
+    --size-m: 12px;
+    --size-l: 16px;
+    --size-xl: 24px;
+    --size-xxl: 32px;
+  }
 
-*{
-  box-sizing: border-box;
-}
+  * {
+    box-sizing: border-box;
+  }
 
-html, body{
-  margin: 0;
-  font-family: Helvetica Arial sans-serif;
-}
+  html, body {
+    margin: 0;
+    font-family: Helvetica Arial sans-serif;
+  }
 
 `

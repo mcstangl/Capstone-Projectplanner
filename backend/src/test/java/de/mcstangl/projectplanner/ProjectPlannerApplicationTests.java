5b8478a82fcbc875d7197afd864dac5bc320d0ac
Matthias Stangl <fazmaz@gmx.de>
2021-09-06 15:52:23 +0200

    Add SpringTestContextConfiguration

--- backend/src/test/java/de/mcstangl/projectplanner/ProjectPlannerApplicationTests.java
+++ backend/src/test/java/de/mcstangl/projectplanner/ProjectPlannerApplicationTests.java
@@ -1,9 +1,12 @@
 package de.mcstangl.projectplanner;
 
+import de.mcstangl.projectplanner.config.SpringTestContextConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = SpringTestContextConfiguration.class)
 class ProjectPlannerApplicationTests {
 
     @Test

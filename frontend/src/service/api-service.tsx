e5d556313d40ebceb738bc433014d40a0ddd88d0
Matthias Stangl <fazmaz@gmx.de>
2021-09-06 10:36:36 +0200

    Add api-service post request for access token

---
+++ frontend/src/service/api-service.tsx
@@ -0,0 +1,7 @@
+import axios from 'axios'
+import { CredentialsDto } from '../dtos/CredentialsDto'
+
+export const getAccessToken = (credentials: CredentialsDto) =>
+  axios
+    .post('/api/project-planner/auth/access_token', credentials)
+    .then(response => response.data)

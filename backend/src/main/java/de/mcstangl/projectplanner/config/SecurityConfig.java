fb650b6ef8fdc47ffc07358213f67a7340417f48
Matthias Stangl <fazmaz@gmx.de>
2021-09-06 12:53:03 +0200

    Add access_token route to security whitelist

--- backend/src/main/java/de/mcstangl/projectplanner/config/SecurityConfig.java
+++ backend/src/main/java/de/mcstangl/projectplanner/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package de.mcstangl.projectplanner.config;
 import de.mcstangl.projectplanner.service.UserEntityDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,9 +25,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userEntityDetailsService);
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
     @Bean
@@ -36,6 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/api/project-planner/auth/access_token").permitAll()
                 .antMatchers("/**").authenticated()
                 .and().formLogin()
                 .and().httpBasic();

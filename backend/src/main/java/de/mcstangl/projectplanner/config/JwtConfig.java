74c4da1849b6014bbd8b406ec16138dae6543148
Matthias Stangl <fazmaz@gmx.de>
2021-09-07 09:19:31 +0200

    Add UserEntity tests

--- backend/src/main/java/de/mcstangl/projectplanner/config/JwtConfig.java
+++ backend/src/main/java/de/mcstangl/projectplanner/config/JwtConfig.java
@@ -1,7 +1,6 @@
 package de.mcstangl.projectplanner.config;
 
 import lombok.Data;
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 

5b9818e3a1c7471699e7fc0b2a329dfadebd9dcd
Matthias Stangl <fazmaz@gmx.de>
2021-09-07 12:07:14 +0200

    Add NewProjectPage

---
+++ frontend/src/pages/NewProjectPage.tsx
@@ -0,0 +1,54 @@
+import { ChangeEvent, FC, FormEvent, useState } from 'react'
+import { PageLayout } from '../components/PageLayout'
+import Header from '../components/Header'
+import styled from 'styled-components/macro'
+
+interface NewProjectDto {
+  customer: string
+  title: string
+}
+
+const NewProjectPage: FC = () => {
+  const [formData, setFormData] = useState<NewProjectDto>({
+    customer: '',
+    title: '',
+  })
+  const submitHandler = (event: FormEvent) => {
+    event.preventDefault()
+    console.log(formData)
+  }
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value })
+  }
+
+  return (
+    <PageLayout>
+      <Header />
+      <ProjectForm onSubmit={submitHandler}>
+        <input
+          name="customer"
+          type="text"
+          placeholder="Kundenname"
+          value={formData.customer}
+          onChange={handleInputChange}
+        />
+        <input
+          name="title"
+          type="text"
+          placeholder="Projekt Titel"
+          value={formData.title}
+          onChange={handleInputChange}
+        />
+        <button>Speichern</button>
+      </ProjectForm>
+    </PageLayout>
+  )
+}
+export default NewProjectPage
+
+const ProjectForm = styled.form`
+  display: grid;
+  grid-gap: var(--size-l);
+  justify-items: center;
+`

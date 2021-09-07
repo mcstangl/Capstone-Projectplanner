36a8f2f728a93821632985163f841daf061e6aa7
Matthias Stangl <fazmaz@gmx.de>
2021-09-07 12:07:36 +0200

    Add Router and Routes to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import LoginPage from './pages/LoginPage'
 import AuthProvider from './auth/AuthProvider'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import NewProjectPage from './pages/NewProjectPage'
 
 function App() {
   return (
     <AuthProvider>
-      <section>
-        <LoginPage />
-      </section>
+      <Router>
+        <Switch>
+          <Route path={'/new-project'}>
+            <NewProjectPage />
+          </Route>
+          <Route path={'/'}>
+            <LoginPage />
+          </Route>
+        </Switch>
+      </Router>
     </AuthProvider>
   )
 }

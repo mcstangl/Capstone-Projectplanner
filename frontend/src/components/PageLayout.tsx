d08d9c2271cf53a0674e331234c2c1fc75fcb1da
Matthias Stangl <fazmaz@gmx.de>
2021-09-07 12:06:21 +0200

    Extract PageLayout

---
+++ frontend/src/components/PageLayout.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components/macro'
+
+export const PageLayout = styled.section`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: grid;
+  grid-template-rows: 170px min-content;
+`
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -3,6 +3,7 @@ import { ChangeEvent, FC, FormEvent, useContext, useState } from 'react'
 import { CredentialsDto } from '../dtos/CredentialsDto'
 import AuthContext from '../auth/AuthContext'
 import Header from '../components/Header'
+import { PageLayout } from '../components/PageLayout'
 
 const LoginPage: FC = () => {
   const { login, authUser } = useContext(AuthContext)
@@ -51,15 +52,6 @@ const LoginPage: FC = () => {
 }
 export default LoginPage
 
-const PageLayout = styled.section`
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  display: grid;
-  grid-template-rows: 170px min-content;
-`
 const LoginForm = styled.form`
   display: grid;
   grid-gap: var(--size-l);

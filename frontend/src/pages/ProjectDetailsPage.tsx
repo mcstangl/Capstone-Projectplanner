15fd505d089558150f864f28a4c5ddee4d174df3
Matthias Stangl <fazmaz@gmx.de>
2021-09-09 18:27:19 +0200

    Add update project logic to ProjectDetailsPage

--- frontend/src/pages/ProjectDetailsPage.tsx
+++ frontend/src/pages/ProjectDetailsPage.tsx
@@ -1,12 +1,20 @@
-import { FC, useContext, useEffect, useState } from 'react'
+import {
+  ChangeEvent,
+  FC,
+  FormEvent,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
 import { PageLayout } from '../components/PageLayout'
 import Header from '../components/Header'
 import { Link, useParams } from 'react-router-dom'
-import { findProjectByTitle } from '../service/api-service'
+import { findProjectByTitle, updateProject } from '../service/api-service'
 import { ProjectDto } from '../dtos/ProjectDto'
 import AuthContext from '../auth/AuthContext'
 import { LinkGroup } from '../components/LinkGroup'
 import styled from 'styled-components/macro'
+import { Button } from '../components/Button'
 
 interface RouteParams {
   projectTitle: string
@@ -17,6 +25,12 @@ const ProjectDetailsPage: FC = () => {
   const { token } = useContext(AuthContext)
 
   const [project, setProject] = useState<ProjectDto>()
+  const [editMode, setEditMode] = useState<boolean>()
+
+  const [formData, setFormData] = useState<ProjectDto>({
+    customer: '',
+    title: '',
+  })
 
   useEffect(() => {
     if (token) {
@@ -26,6 +40,39 @@ const ProjectDetailsPage: FC = () => {
     }
   }, [projectTitle, token])
 
+  useEffect(() => {
+    if (project) {
+      setFormData({ customer: project.customer, title: project.title })
+    }
+  }, [project])
+
+  const submitHandler = (event: FormEvent) => {
+    event.preventDefault()
+    if (project && token) {
+      const updateProjectDto = {
+        title: project.title,
+        newTitle: formData.title,
+        customer: formData.customer,
+      }
+      updateProject(updateProjectDto, token)
+        .then(setProject)
+        .catch(console.error)
+        .finally(() => setEditMode(false))
+    }
+  }
+
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value })
+  }
+
+  const onClickHandler = () => {
+    if (editMode) {
+      setEditMode(false)
+    } else {
+      setEditMode(true)
+    }
+  }
+
   return (
     <PageLayout>
       <Header />
@@ -33,20 +80,46 @@ const ProjectDetailsPage: FC = () => {
         <LinkGroup>
           <Link to="/projects">Zurück zur Liste</Link>
         </LinkGroup>
-        <ProjectDetails>
-          <h4>Kunde</h4> <span>{project?.customer}</span>
-          <h4>Title</h4> <span>{project?.title}</span>
+        <ProjectDetails onSubmit={submitHandler}>
+          <h4>Kunde</h4>
+          {editMode ? (
+            <input
+              name="customer"
+              type="text"
+              value={formData.customer}
+              placeholder={project?.customer}
+              onChange={onChangeHandler}
+            />
+          ) : (
+            <span>{project?.customer}</span>
+          )}
+          <h4>Title</h4>
+          {editMode ? (
+            <input
+              name="title"
+              type="text"
+              value={formData.title}
+              placeholder={project?.title}
+              onChange={onChangeHandler}
+            />
+          ) : (
+            <span>{project?.title}</span>
+          )}
+          {editMode && (
+            <Button type="button" onClick={onClickHandler}>
+              Abbrechen
+            </Button>
+          )}
+          {editMode && <Button>Speichern</Button>}
         </ProjectDetails>
-        <LinkGroup>
-          <Link to="/projects">Editieren</Link>
-        </LinkGroup>
+        {editMode || <Button onClick={onClickHandler}>Edit</Button>}
       </main>
     </PageLayout>
   )
 }
 export default ProjectDetailsPage
 
-const ProjectDetails = styled.section`
+const ProjectDetails = styled.form`
   max-width: 300px;
   display: grid;
   grid-template-columns: min-content 1fr;

e1a435ee27cd5e750e8b0e5f571ae562d80720ee
Matthias Stangl <fazmaz@gmx.de>
2021-09-10 18:43:57 +0200

    Add ownerName to ProjectDto

--- backend/src/main/java/de/mcstangl/projectplanner/api/ProjectDto.java
+++ backend/src/main/java/de/mcstangl/projectplanner/api/ProjectDto.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.util.Objects;
+
 @Data
 @SuperBuilder
 @NoArgsConstructor
@@ -14,5 +16,18 @@ public class ProjectDto {
 
     private String customer;
     private String title;
+    private String ownerName;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProjectDto that = (ProjectDto) o;
+        return Objects.equals(title, that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
 }

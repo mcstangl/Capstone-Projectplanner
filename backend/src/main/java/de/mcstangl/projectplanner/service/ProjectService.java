73164746041728f21590da24146e7d0a1c2b6d5f
Matthias Stangl <fazmaz@gmx.de>
2021-09-13 16:49:21 +0200

    Refactor ProjectService

--- backend/src/main/java/de/mcstangl/projectplanner/service/ProjectService.java
+++ backend/src/main/java/de/mcstangl/projectplanner/service/ProjectService.java
@@ -49,52 +49,81 @@ public class ProjectService {
         return projectRepository.findAll();
     }
 
-    public ProjectEntity update(ProjectEntity projectUpdateEntity, String newTitle) {
+    public ProjectEntity update(ProjectEntity projectUpdateData, String newTitle) {
 
-        ProjectEntity fetchedProjectEntity = findByTitle(projectUpdateEntity.getTitle())
-                .orElseThrow(() -> new EntityNotFoundException(String.format("Projekt mit dem Titel %s konnte nicht gefunden werden", projectUpdateEntity.getTitle())));
+        ProjectEntity fetchedProjectEntity = fetchProjectEntity(projectUpdateData.getTitle());
 
         ProjectEntity projectEntityCopy = copyProjectEntity(fetchedProjectEntity);
 
-        if (projectUpdateEntity.getCustomer() != null) {
-            hasText(projectUpdateEntity.getCustomer(), "Kundenname darf nicht leer sein");
-            projectEntityCopy.setCustomer(projectUpdateEntity.getCustomer().trim());
+        if (projectUpdateData.getCustomer() != null) {
+            updateCustomer(projectUpdateData, projectEntityCopy);
         }
 
-        if (projectUpdateEntity.getOwner() != null && !projectEntityCopy.getOwner().getLoginName().equals(projectUpdateEntity.getOwner().getLoginName())) {
-            projectEntityCopy.setOwner(projectUpdateEntity.getOwner());
+        if (projectUpdateData.getOwner() != null) {
+            projectEntityCopy.setOwner(projectUpdateData.getOwner());
         }
 
-        if (projectUpdateEntity.getWriters() != null) {
-            Set<UserEntity> writersToUpdate = projectUpdateEntity.getWriters();
-            projectEntityCopy.setWriters(new HashSet<>());
-            for (UserEntity writer : writersToUpdate) {
-                UserEntity writerToAdd = userService.findByLoginName(writer.getLoginName())
-                        .orElseThrow(() -> new EntityNotFoundException("Der Benutzer konnte nicht gefunden werden"));
-                projectEntityCopy.addWriter(writerToAdd);
-            }
+        if (projectUpdateData.getWriters() != null) {
+            updateWriters(projectUpdateData, projectEntityCopy);
         }
 
-        if (projectUpdateEntity.getMotionDesigners() != null) {
-            Set<UserEntity> motionDesignersToUpdate = projectUpdateEntity.getMotionDesigners();
-            projectEntityCopy.setMotionDesigners(new HashSet<>());
-
-            for (UserEntity motionDesigner : motionDesignersToUpdate) {
-                UserEntity motionDesignerToAdd = userService.findByLoginName(motionDesigner.getLoginName())
-                        .orElseThrow(() -> new EntityNotFoundException("Der Benutzer konnte nicht gefunden werden"));
-                projectEntityCopy.addMotionDesigner(motionDesignerToAdd);
-            }
+        if (projectUpdateData.getMotionDesigners() != null) {
+            updateMotionDesigners(projectUpdateData, projectEntityCopy);
         }
 
         if (newTitle != null && !newTitle.trim().equals(fetchedProjectEntity.getTitle())) {
-            hasText(newTitle, "Projekttitel darf nicht leer sein");
-            projectEntityCopy.setTitle(newTitle.trim());
-            projectEntityCopy.setId(null);
-            projectRepository.delete(fetchedProjectEntity);
+            updateTitle(newTitle, fetchedProjectEntity, projectEntityCopy);
             return createNewProject(projectEntityCopy);
         }
         return projectRepository.save(projectEntityCopy);
     }
+
+    private ProjectEntity fetchProjectEntity(String title) {
+        return findByTitle(title)
+                .orElseThrow(
+                        () -> new EntityNotFoundException(
+                                String.format(
+                                        "Projekt mit dem Titel %s konnte nicht gefunden werden",
+                                        title
+                                )
+                        )
+                );
+    }
+
+    private void updateCustomer(ProjectEntity projectUpdateEntity, ProjectEntity projectEntityCopy) {
+        hasText(projectUpdateEntity.getCustomer(), "Kundenname darf nicht leer sein");
+        projectEntityCopy.setCustomer(projectUpdateEntity.getCustomer().trim());
+    }
+
+    private void updateWriters(ProjectEntity projectUpdateEntity, ProjectEntity projectEntityCopy) {
+        Set<UserEntity> writersToUpdate = projectUpdateEntity.getWriters();
+        projectEntityCopy.setWriters(new HashSet<>());
+        for (UserEntity writer : writersToUpdate) {
+            UserEntity writerToAdd = userService.findByLoginName(writer.getLoginName())
+                    .orElseThrow(() -> new EntityNotFoundException("Der Benutzer konnte nicht gefunden werden"));
+            projectEntityCopy.addWriter(writerToAdd);
+        }
+    }
+
+
+    private void updateMotionDesigners(ProjectEntity projectUpdateEntity, ProjectEntity projectEntityCopy) {
+        Set<UserEntity> motionDesignersToUpdate = projectUpdateEntity.getMotionDesigners();
+        projectEntityCopy.setMotionDesigners(new HashSet<>());
+
+        for (UserEntity motionDesigner : motionDesignersToUpdate) {
+            UserEntity motionDesignerToAdd = userService.findByLoginName(motionDesigner.getLoginName())
+                    .orElseThrow(() -> new EntityNotFoundException("Der Benutzer konnte nicht gefunden werden"));
+            projectEntityCopy.addMotionDesigner(motionDesignerToAdd);
+        }
+    }
+
+    private void updateTitle(String newTitle, ProjectEntity fetchedProjectEntity, ProjectEntity projectEntityCopy) {
+        hasText(newTitle, "Projekttitel darf nicht leer sein");
+        projectEntityCopy.setTitle(newTitle.trim());
+        projectEntityCopy.setId(null);
+        projectRepository.delete(fetchedProjectEntity);
+    }
+
 
     private ProjectEntity copyProjectEntity(ProjectEntity fetchedProjectEntity) {
         return ProjectEntity.builder()

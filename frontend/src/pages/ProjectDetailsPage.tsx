799ffec6cd8dfaae8e4071134b44c5b244a84151
Matthias Stangl <fazmaz@gmx.de>
2021-09-14 08:51:22 +0200

    Extract ProjectDetails to its own component

--- frontend/src/pages/ProjectDetailsPage.tsx
+++ frontend/src/pages/ProjectDetailsPage.tsx
@@ -6,10 +6,10 @@ import { findProjectByTitle } from '../service/api-service'
 import { ProjectDto } from '../dtos/ProjectDto'
 import AuthContext from '../auth/AuthContext'
 import { LinkGroup } from '../components/LinkGroup'
-import styled from 'styled-components/macro'
 import { Button } from '../components/Button'
 import { RestExceptionDto } from '../dtos/RestExceptionDto'
 import ProjectDetailsEdit from '../components/ProjectDetailsEdit'
+import ProjectDetails from '../components/ProjectDetails'
 
 interface RouteParams {
   projectTitle: string
@@ -58,29 +58,7 @@ const ProjectDetailsPage: FC = () => {
         <LinkGroup>
           <Link to="/projects">Zurück zur Liste</Link>
         </LinkGroup>
-        <ProjectDetails>
-          <h4>Kunde</h4>
-
-          <span>{project?.customer}</span>
-
-          <h4>Title</h4>
-
-          <span>{project?.title}</span>
-
-          <h4>Projektleitung</h4>
-
-          <span>{project?.owner.loginName}</span>
-
-          <h4>Redaktion</h4>
-
-          <span>{project?.writer[0] ? project?.writer[0].loginName : ''}</span>
-
-          <h4>Motion Design</h4>
-
-          <span>
-            {project?.motionDesign[0] ? project?.motionDesign[0].loginName : ''}
-          </span>
-        </ProjectDetails>
+        {!editMode && <ProjectDetails project={project} />}
         {editMode && (
           <ProjectDetailsEdit
             updateErrorState={updateErrorState}
@@ -98,15 +76,3 @@ const ProjectDetailsPage: FC = () => {
   )
 }
 export default ProjectDetailsPage
-
-const ProjectDetails = styled.section`
-  max-width: 300px;
-  display: grid;
-  grid-template-columns: min-content 1fr;
-  grid-gap: var(--size-s);
-
-  h4 {
-    margin: 0;
-    padding: 0;
-  }
-`

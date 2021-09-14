809f3259f856595b5057ed8d81f032d0e388d7fa
Matthias Stangl <fazmaz@gmx.de>
2021-09-14 12:37:19 +0200

    Add dateOfReceipt to Dtos

--- frontend/src/dtos/UpdateProjectDto.tsx
+++ frontend/src/dtos/UpdateProjectDto.tsx
@@ -4,6 +4,7 @@ export interface UpdateProjectDto {
   customer: string
   title: string
   newTitle: string
+  dateOfReceipt: string
   owner: UserDto
   writer: UserDto[]
   motionDesign: UserDto[]

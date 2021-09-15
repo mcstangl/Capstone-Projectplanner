dc0178ced0a74acf9bf4e2f192d6d14272a1dcba
Matthias Stangl <fazmaz@gmx.de>
2021-09-15 10:24:35 +0200

    Add createNewMileStone logic and test

--- backend/src/main/java/de/mcstangl/projectplanner/controller/MileStoneController.java
+++ backend/src/main/java/de/mcstangl/projectplanner/controller/MileStoneController.java
@@ -2,15 +2,20 @@ package de.mcstangl.projectplanner.controller;
 
 import de.mcstangl.projectplanner.api.MileStoneDto;
 import de.mcstangl.projectplanner.model.MileStoneEntity;
+import de.mcstangl.projectplanner.model.ProjectEntity;
 import de.mcstangl.projectplanner.service.MileStoneService;
+import de.mcstangl.projectplanner.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
+import java.sql.Date;
 import java.util.LinkedList;
 import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.util.Assert.hasText;
 
 @CrossOrigin
 @RestController
@@ -18,10 +23,12 @@ import static org.springframework.http.ResponseEntity.ok;
 public class MileStoneController {
 
     private final MileStoneService mileStoneService;
+    private final ProjectService projectService;
 
     @Autowired
-    public MileStoneController(MileStoneService mileStoneService) {
+    public MileStoneController(MileStoneService mileStoneService, ProjectService projectService) {
         this.mileStoneService = mileStoneService;
+        this.projectService = projectService;
     }
 
     @GetMapping("{projectTitle}")
@@ -30,6 +37,24 @@ public class MileStoneController {
         return ok(map(mileStoneEntityList));
     }
 
+    @PostMapping
+    public ResponseEntity<MileStoneDto> createNewMilestone(@RequestBody MileStoneDto mileStoneDto){
+
+        hasText(mileStoneDto.getTitle(), "Ein Milestone muss einen Titel haben");
+
+        ProjectEntity projectEntity = projectService.findByTitle(mileStoneDto.getProjectTitle())
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("Projekt mit dem Titel %s konnte nicht gefunden werden", mileStoneDto.getProjectTitle())));
+
+        MileStoneEntity mileStoneEntity = map(mileStoneDto);
+        mileStoneEntity.setProjectEntity(projectEntity);
+
+        MileStoneEntity newMileStone = mileStoneService.createNewMileStone(mileStoneEntity);
+
+        return ok(map(newMileStone));
+    }
+
+
     private List<MileStoneDto> map(List<MileStoneEntity> mileStoneEntityList) {
         List<MileStoneDto> mileStoneDtoList = new LinkedList<>();
         for (MileStoneEntity mileStoneEntity : mileStoneEntityList) {
@@ -37,12 +62,22 @@ public class MileStoneController {
         }
         return mileStoneDtoList;
     }
+    private MileStoneEntity map(MileStoneDto mileStoneDto) {
+        return MileStoneEntity.builder()
+                .title(mileStoneDto.getTitle())
+                .dueDate(Date.valueOf(mileStoneDto.getDueDate()))
+                .dateFinished(Date.valueOf(mileStoneDto.getDateFinished()))
+                .build();
+    }
 
     private MileStoneDto map(MileStoneEntity mileStoneEntity) {
         return MileStoneDto.builder()
                 .title(mileStoneEntity.getTitle())
                 .dueDate(mileStoneEntity.getDueDate().toString())
+                .projectTitle(mileStoneEntity.getProjectEntity().getTitle())
                 .dateFinished(mileStoneEntity.getDateFinished().toString())
                 .build();
     }
+
+
 }
--- backend/src/test/java/de/mcstangl/projectplanner/controller/MileStoneControllerTest.java
+++ backend/src/test/java/de/mcstangl/projectplanner/controller/MileStoneControllerTest.java
@@ -11,6 +11,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -18,6 +21,8 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
 
 import java.sql.Date;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -77,23 +82,22 @@ class MileStoneControllerTest extends SpringBootTests {
                         .title("Test")
                         .build()
         );
-        testMilestone2 = MileStoneEntity.builder()
-                .id(2L)
-                .projectEntity(testProject2)
-                .dateFinished(Date.valueOf("2021-12-12"))
-                .dueDate(Date.valueOf("2021-03-13"))
-                .title("Test2")
-                .build();
-
-        testMilestone3 = mileStoneRepository.saveAndFlush(
+        testMilestone2 = mileStoneRepository.saveAndFlush(
                 MileStoneEntity.builder()
-                        .id(3L)
+                        .id(2L)
                         .projectEntity(testProject1)
                         .dateFinished(Date.valueOf("2021-12-12"))
                         .dueDate(Date.valueOf("2021-03-13"))
                         .title("Test")
                         .build()
         );
+        testMilestone3 = MileStoneEntity.builder()
+                .projectEntity(testProject2)
+                .dateFinished(Date.valueOf("2021-12-12"))
+                .dueDate(Date.valueOf("2021-03-13"))
+                .title("Test3")
+                .build();
+
 
     }
 
@@ -107,20 +111,77 @@ class MileStoneControllerTest extends SpringBootTests {
     @Test
     @DisplayName("Find by project title should return all milestones related the project")
     public void findAllByProjectTitle() {
-        // Given
-        String projectTitle = "Test1";
-        HttpHeaders httpHeaders = testUtil.getAuthHeader("USER");
         // When
         ResponseEntity<MileStoneDto[]> response = testRestTemplate.exchange(
-                getUrl() + "/" + projectTitle,
+                getUrl() + "/Test1",
                 HttpMethod.GET,
-                new HttpEntity<>(null, httpHeaders),
+                new HttpEntity<>(null, testUtil.getAuthHeader("USER")),
                 MileStoneDto[].class);
 
         // Then
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
         assertNotNull(response.getBody());
         assertThat(response.getBody().length, is(2));
+    }
+
+    @Test
+    @DisplayName("Create new milestone should return the milestone created")
+    public void createNewMilestone(){
+        // Given
+        MileStoneDto mileStoneDto = MileStoneDto.builder()
+                .projectTitle("Test1")
+                .title("Test")
+                .dueDate("2021-03-13")
+                .dateFinished("2021-12-12")
+                .build();
+
+        // When
+        ResponseEntity<MileStoneDto> response = testRestTemplate.exchange(
+                getUrl(),
+                HttpMethod.POST,
+                new HttpEntity<>(mileStoneDto, testUtil.getAuthHeader("ADMIN")),
+                MileStoneDto.class);
+
+        // Then
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertNotNull(response.getBody());
+        assertThat(response.getBody().getTitle(),is("Test"));
+        assertThat(response.getBody().getDateFinished(),is("2021-12-12"));
+        assertThat(response.getBody().getDueDate() ,is("2021-03-13"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getArgumentsForCreateNewMilestoneTest")
+    @DisplayName("Create new milestone without title or unknown project title should fail")
+    public void createNewMilestone(String title, String projectTitle, HttpStatus httpStatus){
+        // Given
+        MileStoneDto mileStoneDto = MileStoneDto.builder()
+                .projectTitle(projectTitle)
+                .title(title)
+                .dueDate("2021-03-13")
+                .dateFinished("2021-12-12")
+                .build();
+
+        // When
+        ResponseEntity<MileStoneDto> response = testRestTemplate.exchange(
+                getUrl(),
+                HttpMethod.POST,
+                new HttpEntity<>(mileStoneDto, testUtil.getAuthHeader("ADMIN")),
+                MileStoneDto.class);
+
+        // Then
+        assertThat(response.getStatusCode(), is(httpStatus));
+    }
+
+
+    private static Stream<Arguments> getArgumentsForCreateNewMilestoneTest(){
+        return Stream.of(
+                Arguments.of(null, "Test1", HttpStatus.BAD_REQUEST),
+                Arguments.of("", "Test1", HttpStatus.BAD_REQUEST),
+                Arguments.of("Test", "Unknown", HttpStatus.NOT_FOUND),
+                Arguments.of("Test", null , HttpStatus.NOT_FOUND)
+
+        );
     }
 
 

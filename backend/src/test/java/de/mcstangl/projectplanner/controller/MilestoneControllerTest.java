8120d869951bde209fb6df79673dc46bdf5a6240
Matthias Stangl <fazmaz@gmx.de>
2021-09-17 11:00:32 +0200

    Add test cases for update entity

--- backend/src/test/java/de/mcstangl/projectplanner/controller/MilestoneControllerTest.java
+++ backend/src/test/java/de/mcstangl/projectplanner/controller/MilestoneControllerTest.java
@@ -107,7 +107,7 @@ class MilestoneControllerTest extends SpringBootTests {
         // Given
         MilestoneDto mileStoneDto = MilestoneDto.builder()
                 .projectTitle("Test1")
-                .title("Test")
+                .title("New Milestone")
                 .dueDate("2021-03-13")
                 .dateFinished("2021-12-12")
                 .build();
@@ -122,7 +122,8 @@ class MilestoneControllerTest extends SpringBootTests {
         // Then
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
         assertNotNull(response.getBody());
-        assertThat(response.getBody().getTitle(),is("Test"));
+        assertNotNull(response.getBody().getId());
+        assertThat(response.getBody().getTitle(),is("New Milestone"));
         assertThat(response.getBody().getDateFinished(),is("2021-12-12"));
         assertThat(response.getBody().getDueDate() ,is("2021-03-13"));
     }
@@ -191,7 +192,7 @@ class MilestoneControllerTest extends SpringBootTests {
         // Given
         MilestoneDto mileStoneDto = MilestoneDto.builder()
                 .projectTitle("Test1")
-                .title("Test")
+                .title("New Milestone")
                 .dueDate(dueDate)
                 .dateFinished(dateFinished)
                 .build();
@@ -221,6 +222,130 @@ class MilestoneControllerTest extends SpringBootTests {
         );
     }
 
+    @Test
+    @DisplayName("Update milestone should return the milestone created")
+    public void updateMilestone(){
+        // Given
+        MilestoneDto mileStoneDto = MilestoneDto.builder()
+                .id(1L)
+                .projectTitle("Test1")
+                .title("Updated Milestone")
+                .dueDate("2021-01-01")
+                .dateFinished("2021-01-01")
+                .build();
+
+        // When
+        ResponseEntity<MilestoneDto> response = testRestTemplate.exchange(
+                getUrl(),
+                HttpMethod.PUT,
+                new HttpEntity<>(mileStoneDto, testUtil.getAuthHeader("ADMIN")),
+                MilestoneDto.class);
+
+        // Then
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertNotNull(response.getBody());
+        assertThat(response.getBody().getProjectTitle(), is("Test1"));
+        assertThat(response.getBody().getTitle(),is("Updated Milestone"));
+        assertThat(response.getBody().getDateFinished(),is("2021-01-01"));
+        assertThat(response.getBody().getDueDate() ,is("2021-01-01"));
+    }
+
+    @Test
+    @DisplayName("Update new milestone as user should fail")
+    public void updateMilestoneAsUser(){
+        // Given
+        MilestoneDto mileStoneDto = MilestoneDto.builder()
+                .id(1L)
+                .projectTitle("Test1")
+                .title("Test")
+                .dueDate("2021-03-13")
+                .dateFinished("2021-12-12")
+                .build();
+
+        // When
+        ResponseEntity<MilestoneDto> response = testRestTemplate.exchange(
+                getUrl(),
+                HttpMethod.PUT,
+                new HttpEntity<>(mileStoneDto, testUtil.getAuthHeader("USER")),
+                MilestoneDto.class);
+
+        // Then
+        assertThat(response.getStatusCode(), is(HttpStatus.UNAUTHORIZED));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getArgumentsForUpdateNewMilestoneTest")
+    @DisplayName("Update milestone without title or unknown project title should fail")
+    public void updateMilestoneWithoutTitle(Long id, String title, String projectTitle, HttpStatus httpStatus){
+        // Given
+        MilestoneDto mileStoneDto = MilestoneDto.builder()
+                .id(id)
+                .projectTitle(projectTitle)
+                .title(title)
+                .dueDate("2021-03-13")
+                .dateFinished("2021-12-12")
+                .build();
+
+        // When
+        ResponseEntity<MilestoneDto> response = testRestTemplate.exchange(
+                getUrl(),
+                HttpMethod.PUT,
+                new HttpEntity<>(mileStoneDto, testUtil.getAuthHeader("ADMIN")),
+                MilestoneDto.class);
+
+        // Then
+        assertThat(response.getStatusCode(), is(httpStatus));
+    }
+
+
+    private static Stream<Arguments> getArgumentsForUpdateNewMilestoneTest(){
+        return Stream.of(
+                Arguments.of(1L, null, "Test1", HttpStatus.BAD_REQUEST),
+                Arguments.of(null, "Test", "Test1", HttpStatus.BAD_REQUEST),
+                Arguments.of(1L, "", "Test1", HttpStatus.BAD_REQUEST),
+                Arguments.of(1L, "Test", "Unknown", HttpStatus.NOT_FOUND),
+                Arguments.of(1L, "Test", null , HttpStatus.NOT_FOUND)
+
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getArgumentsForUpdateMilestoneWithBadDatesTest")
+    @DisplayName("Update milestone with a bad date should set the date to null")
+    public void updateMilestoneWithBadDates(String dueDate, String dateFinished, String expectedDueDate, String expectedDateFinished){
+        // Given
+        MilestoneDto mileStoneDto = MilestoneDto.builder()
+                .id(1L)
+                .projectTitle("Test1")
+                .title("New Milestone")
+                .dueDate(dueDate)
+                .dateFinished(dateFinished)
+                .build();
+
+        // When
+        ResponseEntity<MilestoneDto> response = testRestTemplate.exchange(
+                getUrl(),
+                HttpMethod.PUT,
+                new HttpEntity<>(mileStoneDto, testUtil.getAuthHeader("ADMIN")),
+                MilestoneDto.class);
+
+        // Then
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertNotNull(response.getBody());
+        assertThat(response.getBody().getDueDate(), is(expectedDueDate));
+        assertThat(response.getBody().getDateFinished(), is(expectedDateFinished));
+
+    }
+
+
+    private static Stream<Arguments> getArgumentsForUpdateMilestoneWithBadDatesTest(){
+        return Stream.of(
+                Arguments.of(null, "2001-12-12", null, "2001-12-12"),
+                Arguments.of("2001-12-12", null, "2001-12-12", null ),
+                Arguments.of("noDate", "2001-12-12", null, "2001-12-12"),
+                Arguments.of("2001-12-12", "noDate", "2001-12-12", null )
+        );
+    }
 
     private String getUrl() {
         return String.format("http://localhost:%s/api/project-planner/milestone", port);

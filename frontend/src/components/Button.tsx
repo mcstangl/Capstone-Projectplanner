319bd19b0cbd4ec11fd731f9cdd12c572ccae1b8
Matthias Stangl <fazmaz@gmx.de>
2021-09-21 15:29:10 +0200

    Add secondary button style

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components/macro'
+import styled, { css } from 'styled-components/macro'
 
 export const Button = styled.button`
   padding: 0.5rem 2rem;
@@ -8,9 +8,27 @@ export const Button = styled.button`
   font-size: 1em;
   border-radius: var(--size-xs);
 
+  ${props =>
+    props.theme === 'secondary'
+      ? css`
+          border: 1px solid var(--accentcolor);
+          color: var(--accentcolor);
+        `
+      : css`
+          border: 1px solid var(--maincolor);
+          color: var(--maincolor);
+        `}
   &:hover {
-    background-color: var(--maincolor);
-    color: white;
+    ${props =>
+      props.theme === 'secondary'
+        ? css`
+            background-color: var(--accentcolor);
+            color: white;
+          `
+        : css`
+            background-color: var(--maincolor);
+            color: white; ;
+          `}
   }
 
   :disabled {
